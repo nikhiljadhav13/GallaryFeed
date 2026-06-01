bff95a5375ea2aa2f0de52420ea40e55c300d535
nikhiljadhav13 <115921099+nikhiljadhav13@users.noreply.github.com>
2026-06-01 16:01:36 +0530

    accessed the image in with the help of postman using the multer middle ware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const multer = require('multer')
 
 
 const app = express()
+app.use(express.json())   // middle ware( this ware for text)
 
+const upload = multer({storage: multer.memoryStorage()}) // this multer middle ware is for reading files
+
+
+
+app.post('/create-post', upload.single("image-1"), (req,res)=>{
+
+    console.log(req.body)
+    console.log(req.file)
+})
 
 module.exports = app
