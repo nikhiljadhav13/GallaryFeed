9f9c4d4fe71bf65a37d999c6c98157e31ff19196
nikhiljadhav13 <115921099+nikhiljadhav13@users.noreply.github.com>
2026-06-02 18:06:15 +0530

    GallaryFeed is done :)

--- Frontend/src/pages/Createpost.jsx
+++ Frontend/src/pages/Createpost.jsx
@@ -3,7 +3,6 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 
-
 const Createpost = () => {
 
   const navigate = useNavigate()  // this is for navigation ( this is for navigating to feed page after creating post)
